7e5649a92e53241b0127174921f0c1f941549d08
s2059232 <s2059232>
2021-03-30 11:06:38 +0100

    PHP: Add Roll Up Button

---
+++ RollButton.php
@@ -0,0 +1,9 @@
+<?php
+echo <<<_ROLLBUTTON
+<div class="scroll-top-wrapper ">
+	<span class="scroll-top-inner">
+		<span class="glyphicon glyphicon-chevron-up" aria-hidden="true"></span>
+	</span>
+</div>
+_ROLLBUTTON;
+?>
