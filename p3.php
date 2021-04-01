d3095adecc1ddb23c53145af697db454a5cad7f8
s2059232 <s2059232>
2021-04-01 04:42:51 +0100

    PHP: style p3

--- p3.php
+++ p3.php
@@ -1,19 +1,48 @@
 <?php
+// --------------- The Shared Part For All Pages: Navbar and HEAD information --------------- //
+// Start the page and ensure the login and redirect method.
 session_start();
-include 'redir.php';
 require_once 'login.php';
+include 'redir.php';
+
+// The HEAD information of the website and load the css style
 echo<<<_HEAD1
-<html>
+<html lang="en">
+
+<!-- Metadata and css style for all pages -->
+<head>
+    <!-- Define the Encoding system -->
+    <meta charset="utf-8">
+    <!-- Bootstrap Core CSS -->
+    <link href="http://mscidwd.bch.ed.ac.uk/s2059232/webDD-ICA/CSS/bootstrap.css" rel="stylesheet">
+    <!-- Customise CSS -->
+    <link href="http://mscidwd.bch.ed.ac.uk/s2059232/webDD-ICA/CSS/carousel.css" rel="stylesheet">
+</head>
 <body>
 _HEAD1;
+
+// Nav Bar
 include 'menuf.php';
+
+// --------------- The Shared Part For All Pages: Navbar and HEAD information  --------------- //
+
+// --------------- Features for Different Pages: Statistics --------------- //
+// Main Content 1
+echo <<<_CONTENTS1
+<div class="mycontainer" id="mycontentcon">
+  <div class="row">
+
+    <div class="col-md-9" id="mycontent">
+      <div>
+        <h1 class="">Statistics</h1>
+        <hr class="col-md-12">
+        <h2>“Plans for the drug administration mode”</h2> 
+_CONTENTS1;
+
+// Attributes Array
 $dbfs = array("natm","ncar","nnit","noxy","nsul","ncycl","nhdon","nhacc","nrotb","mw","TPSA","XLogP");
 $nms = array("n atoms","n carbons","n nitrogens","n oxygens","n sulphurs","n cycles","n H donors","n H acceptors","n rot bonds","mol wt","TPSA","XLogP");
-echo <<<_MAIN1
-    <pre>
-This is the Statistics Page  (not Complete)
-    </pre>
-_MAIN1;
+
 if(isset($_POST['tgval'])) 
    {
      $chosen = 0;
@@ -31,6 +60,31 @@ if(isset($_POST['tgval']))
      if(!$result) die("unable to process query: " . mysql_error());
      $row = mysql_fetch_row($result);
      printf(" Average %f  Standard Dev %f <br />\n",$row[0],$row[1]);
+     $smask = $_SESSION['supmask'];
+     $firstmn = False;
+     $mansel = "(";
+     for($j = 0 ; $j < $rows ; ++$j) {              #Figure out the manufacturer clause for the where statement
+       $row = mysql_fetch_row($result);
+       $sid[$j] = $row[0];
+       $snm[$j] = $row[1];
+       $sact[$j] = 0;
+       $tvl = 1 << ($sid[$j] - 1);
+       if($tvl == ($tvl & $smask)) {
+         $sact[$j] = 1;
+         if($firstmn) $mansel = $mansel." or ";
+         $firstmn = True;
+         $mansel = $mansel." (ManuID = ".$sid[$j].")";
+       }
+     }
+     $mansel = $mansel.")";
+     $comtodo = "./python/histog.py ".$dbfs[$chosen]." \"".$nms[$chosen]."\" \"".$mansel."\"";
+     $output = base64_encode(shell_exec($comtodo));
+
+     echo <<<_imgput
+     <pre>
+     <img  src="data:image/png;base64,$output" />                                              
+     </pre>
+_imgput;
    }
 
 echo '<form action="p3.php" method="post"><pre>';
@@ -46,9 +100,43 @@ for($j = 0 ; $j <sizeof($dbfs) ; ++$j) {
 echo '<input type="submit" value="OK" />';
 echo '</pre></form>';
 
+
+// Main Content 2
+echo <<<_CONTENTE1
+    <p>&nbsp;</p>
+      <h3>References</h3>
+        <p>[1] Sepantafar, Mohammadmajid, et al. “Engineered Hydrogels in Cancer Therapy and Diagnosis.” Trends in Biotechnology 35.11(2017).</p>
+        <p>[2] Park, Semi , et al. "Benefits of Recurrent Colonic Stent Insertion in a Patient with Advanced Gastric Cancer with Carcinomatosis Causing Colonic Obstruction." Yonsei Medical Journal 50.2(2009).</p>
+        <p>[3] English, Max A., et al. "Programmable CRISPR-responsive smart materials." Science 365.6455 (2019): 780-785.</p>
+      </div>
+      <hr class="col-md-13">
+    </div>
+  </div>
+</div>
+</div>
+_CONTENTE1;
+
+// --------------- Features for Different Pages: Statistics --------------- //
+
+// --------------- The Shared Part For All Pages: Roll up button, Footer and JavaScirpt Files --------------- //
+// Roll Up Button
+include 'rollbutton.php';
+
+// Footer
+include 'footer.php';
+
+// Tail for the HTML
 echo <<<_TAIL1
+<!-- --------------- Bootstrap Core JavaScript --------------- -->
+<script src="http://mscidwd.bch.ed.ac.uk/s2059232/webDD-ICA/JS/jquery.min.js"></script>
+<!-- <script src="../carousel/js/jquery.js"></script> -->
+<script src="http://mscidwd.bch.ed.ac.uk/s2059232/webDD-ICA/JS/bootstrap.js"></script>
+<!-- Just to make our placeholder images work. Don't actually copy the next line! -->
+<script src="http://mscidwd.bch.ed.ac.uk/s2059232/webDD-ICA/JS/holder.min.js"></script>
+
 </body>
 </html>
 _TAIL1;
 
+// --------------- The Shared Part For All Pages: Roll up button, Footer and JavaScirpt Files --------------- //
 ?>
